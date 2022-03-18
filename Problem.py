f37ecf75f0b56b3c44a76a75b3e87ea8a7866f15
Revek312 <93083745+PataluchJ@users.noreply.github.com>
2022-03-18 11:24:47 +0100

    Added loading instances from files + refactoring

--- Problem.py
+++ Problem.py
@@ -1,90 +1,116 @@
 import random
 import math
+import fileloader
 
 class Problem:
     def __init__(self):
-        self.adjacencyMatrix = None
-        self.solution = None
-        self.optimalSolution = None
-        self.points = None
-        self.valid = False
+        self.__adjacencyMatrix = None
+        self.__solution = None
+        self.__optimalSolution = None
+        self.__points = None
+        self.__loader = fileloader.FileLoader()
 
-    def loadProblemFromFile(self, filename):
-        pass
+    def loadProblemFromFile(self, filename: str) -> None:
+        self.__adjacencyMatrix = self.__loader.loadProblemInstance(filename)
 
-    def loadSolutionFromFile(self, filename):
-        pass
+    def loadSolutionFromFile(self, filename: str) -> None:
+        self.__optimalSolution = self.__loader.loadOptimalSolution(filename)
 
-    def printAdjecency(self):
-        if self.adjacencyMatrix == None:
+    def printAdjecency(self) -> None:
+        if self.__adjacencyMatrix == None:
             print("Empty")
             return
 
-        width = len(self.adjacencyMatrix)
-        for y in range(width):
-            for x in range(width):
-                print(self.adjacencyMatrix[y][x], end=' ')
+        height = len(self.__adjacencyMatrix)
+        if height < 10:
+            for y in range(height):
+                for x in range(height):
+                    print(self.__adjacencyMatrix[y][x], end=' ')
+                print('')
             print('')
-        print('')
+        else:
+            cellsToPrint = [0,1,2,3,height-3,height-2,height-1]
+            for y in cellsToPrint:
+                for x in cellsToPrint:
+                    print('{0:03}'.format(self.__adjacencyMatrix[y][x]), end=' ')
+                    if x == 3:
+                        print(' ... ', end='')
+                print('')
+                if y == 3:
+                    print(' ... ')
 
-    def _printArray(self, array):
+    def __printArray(self, array: list[int]) -> None:
         for i in range(len(array)):
             print(array[i], end=' ')
         print('')
 
-    def printSolution(self):
-        if self.solution == None:
+    def printSolution(self) -> None:
+        if self.__solution == None:
             print("Problem not yet solved.")
             return
-        if self.points != None:
+        if self.__points != None:
             pass
-        else: 
-            self._printArray(self.solution)
+        self.__printArray(self.__solution)
+        print('Weight: ', self.calcSolutionLen(self.__solution))
         
-    def printOptimalSolution(self):
-        if self.optimalSolution == None:
+    def printOptimalSolution(self) -> None:
+        if self.__optimalSolution == None:
             print("No optimal solution provided.")
             return
-        self._printArray(self.optimalSolution)
+        self.__printArray(self.__optimalSolution)
+        print('Weight: ', self.calcSolutionLen(self.__optimalSolution))
 
-    def calcSolutionLen(self, solution):
+    def calcSolutionLen(self, solution: list[int]) -> int:
         distance = 0
         for i in range(len(solution)-1):
-            distance += self.adjacencyMatrix[solution[i]][solution[i+1]]
-        distance += self.adjacencyMatrix[solution[len(solution)-1]][solution[0]]
+            #print("D: ", solution[i], "->", solution[i+1], " = ", end=' ')
+            print( self.__adjacencyMatrix[solution[i]][solution[i+1]])
+            distance += self.__adjacencyMatrix[solution[i]][solution[i+1]]
+        #print("D: ", solution[len(solution)-1], "->", solution[0], " = ", self.__adjacencyMatrix[solution[len(solution)-1]][solution[0]])
+        distance += self.__adjacencyMatrix[solution[len(solution)-1]][solution[0]]
         return distance
 
-    def PRD(self):
-        if self.solution == None:
+    def PRD(self) -> int:
+        if self.__solution == None:
             print("No solution provided.")
             return
-        if self.optimalSolution == None:
+        if self.__optimalSolution == None:
             print("No optimal solution provided.")
             return 
-        fx = self.calcSolutionLen(self.solution)
-        fref = self.calcSolutionLen(self.optimalSolution)
+        fx = self.calcSolutionLen(self.__solution)
+        fref = self.calcSolutionLen(self.__optimalSolution)
         return (fx-fref)/fref
 
-    def generateAsAsymetric(self, size, seed, minimumWeight, maximumWeight):
+    def generateAsAsymetric(self, size: int, seed, minimumWeight: int, maximumWeight: int) -> None:
         random.seed(seed)
         self.valid = True
-        self.adjacencyMatrix = [[random.randint(minimumWeight,maximumWeight) for x in range(size)] for y in range(size)]
+        self.__adjacencyMatrix = [[random.randint(minimumWeight,maximumWeight) for x in range(size)] for y in range(size)]
         for i in range(size):
-            self.adjacencyMatrix[i][i] = 0
+            self.__adjacencyMatrix[i][i] = 0
 
-    def generateAsSymteric(self, size, seed,minimumWeight, maximumWeight):
+    def generateAsSymteric(self, size: int, seed, minimumWeight: int, maximumWeight: int) -> None:
         self.generateAsymetric(size, seed, minimumWeight, maximumWeight)
         for x in range(0,size):
             for y in range(x+1, size):
-                self.adjacencyMatrix[x][y] = self.adjacencyMatrix[y][x]
+                self.__adjacencyMatrix[x][y] = self.__adjacencyMatrix[y][x]
 
-    def generateAsEuclid2D(self, size, seed, width):
+    def generateAsEuclid2D(self, size: int, seed, width: int) -> None:
         random.seed(seed)
         self.valid = True
-        self.points = [[random.random()*width for i in range(2)] for j in range(size)]
-        self.adjacencyMatrix = [[0 for x in range(size)] for y in range(size)]
+        self.__points = [[random.random()*width for i in range(2)] for j in range(size)]
+        self.__adjacencyMatrix = [[0 for x in range(size)] for y in range(size)]
         for p in range(size):
             for o in range(size):
-                self.adjacencyMatrixtrix[p][o] = round(math.sqrt(
-                    math.pow(self.points[p][0] - self.points[o][0],2) + math.pow(self.points[p][1] - self.points[o][1],2)
+                self.__adjacencyMatrixtrix[p][o] = round(math.sqrt(
+                    math.pow(self.__points[p][0] - self.__points[o][0],2) + math.pow(self.__points[p][1] - self.__points[o][1],2)
                 ))
+
+def test():
+    p = Problem()
+    #p.loadSolutionFromFile("gr120.opt.tour")
+    p.loadProblemFromFile("gr120.tsp")
+    p.loadSolutionFromFile("gr120.opt.tour")
+    p.printOptimalSolution()
+
+if __name__ == "__main__":
+    test()
